dcad9235e66de54b0bf451206c4b1873e95bc4d3
wcjameson <williamjameson90@gmail.com>
2022-08-10 18:27:04 -0700

    add some initial routing and navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import Header from './Header';
 import GetAddressStats from './GetAddressStats';
 import GetTxStatus from './GetTxStatus';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="address">Address</Link>
+        <Link to="tx">Transaction</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/address" element={<GetAddressStats />} />
+        <Route path="/tx" element={<GetTxStatus />} />
+      </Routes>
+      {/* <Header />
       <GetAddressStats />
-      <GetTxStatus />
-    </div>
+      <GetTxStatus /> */}
+    </Router>
   );
 }
 
